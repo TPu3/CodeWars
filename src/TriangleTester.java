75c00490f9162b9c26fe4e352e1495641ac3cc76
TPu3 <46763102+TPu3@users.noreply.github.com>
2019-04-08 22:09:48 +0300

    add time check

--- src/TriangleTester.java
+++ src/TriangleTester.java
@@ -6,9 +6,11 @@ public class TriangleTester {
 
 class TestTriangleTester {
     public static void main(String[] args) {
-
+        double startTimeStSort = System.nanoTime();
 
         System.out.println(TriangleTester.isTriangle(3,4,5));
+        double endTimeStSort = System.nanoTime();
+        System.out.println((startTimeStSort- endTimeStSort)/1e9);
         System.out.println(TriangleTester.isTriangle(-1,-2,-3));
     }
 }
